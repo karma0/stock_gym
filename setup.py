cb62f61a8a408792bf075cc739bf168ee3ecb937
Bobby <karmma0@gmail.com>
2018-01-12 13:34:59 -0700

    Fixed setup.py, swap entrypoint for module

--- setup.py
+++ setup.py
@@ -12,25 +12,17 @@ dependency_links = [
     'https://github.com/openai/gym'
 ]
 
-install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-]
 
-
-setup(name='stock_gym',
+setup(
+    name='stock_gym',
+    packages=['stock_gym'],
     version=version,
     description="An OpenAI Gym environment for stock market data",
     long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='openai-gym-environments,openai-gym,openai',
+    classifiers=[],
+    keywords='openai-gym-environments openai-gym openai',
     author='Bobby',
     author_email='karma0@gmail.com',
-    url='https://github.com/karma0/stock_gym.git',
+    url='https://github.com/karma0/stock_gym',
     license='MIT',
-    packages=['stock_gym'],
-    install_requires=install_requires,
 )
